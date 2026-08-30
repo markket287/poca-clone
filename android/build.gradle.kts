aaa9705fb0e78518f82d3d8245f485618ae662e2
আপনার GitHub নাম <foyjulislam38@gmail.com>
2026-08-29 23:16:38 -0700

    Fix build setup and push for final APK

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    id("com.android.application") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
